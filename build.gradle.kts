5753636186965c5cb8a18f9badafd75c80ed2a75
Samuel Lebailly <sam.meusa@gmail.com>
2025-02-26 13:07:25 +0100

    chore(RA-52): Ajout au niveau du projet, du plugin Sonarqube pour le code coverage.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,5 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     id("com.google.dagger.hilt.android") version "2.51.1" apply false
+    id("org.sonarqube") version "5.1.0.4882" apply false
 }
