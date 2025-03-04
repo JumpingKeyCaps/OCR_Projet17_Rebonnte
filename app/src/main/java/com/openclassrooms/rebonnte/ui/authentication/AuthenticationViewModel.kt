b96e19613331ad6d9c8871655e74a6dece12eac9
Samuel Lebailly <sam.meusa@gmail.com>
2025-03-04 14:39:52 +0100

    feature(RA-67): Create the Authentication ViewModel with the injected authentication repository,and add private/exposed sharedFlow + add the register user method.

---
+++ app/src/main/java/com/openclassrooms/rebonnte/ui/authentication/AuthenticationViewModel.kt
@@ -0,0 +1,49 @@
+package com.openclassrooms.rebonnte.ui.authentication
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.firebase.auth.FirebaseUser
+import com.openclassrooms.rebonnte.data.repository.AuthenticationRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+/**
+ * ViewModel for authentication-related operations.
+ */
+@HiltViewModel
+class AuthenticationViewModel @Inject constructor(private val authRepository: AuthenticationRepository) : ViewModel()  {
+
+    private val _registerResult = MutableSharedFlow<Result<FirebaseUser?>>()
+    val registerResult: SharedFlow<Result<FirebaseUser?>> get() = _registerResult
+
+    private val _signInResult = MutableSharedFlow<Result<FirebaseUser?>>()
+    val signInResult: SharedFlow<Result<FirebaseUser?>> get() = _signInResult
+
+    /**
+     * Registers a new user with the provided email and password.
+     * @param email The email address of the new user.
+     * @param password The password for the new user.
+     * @param firstName The first name of the new user.
+     * @param lastName The last name of the new user.
+     */
+    fun registerUser(email: String, password: String, firstName: String, lastName: String) {
+        viewModelScope.launch {
+            authRepository.registerUser(email, password)
+                .collect { result ->
+                    //Check is the user is successfully added
+                    if (result.isSuccess) {
+                        val user = result.getOrNull()
+                        if (user != null) {
+                            //todo :::  call here the User repository to add the user in DataBase
+                        }
+                        _registerResult.emit(result)
+                    }
+                }
+        }
+    }
+
+
+}
