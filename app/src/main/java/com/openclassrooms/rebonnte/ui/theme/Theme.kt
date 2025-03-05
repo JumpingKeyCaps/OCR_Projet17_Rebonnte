4aef54da8b798a0c877af4036df3b991a2c0d527
Samuel Lebailly <sam.meusa@gmail.com>
2025-03-05 13:04:25 +0100

    feature(RA-68):Set App theme to darkmode only + Correct app name error.

--- app/src/main/java/com/openclassrooms/rebonnte/ui/theme/Theme.kt
+++ app/src/main/java/com/openclassrooms/rebonnte/ui/theme/Theme.kt
@@ -9,35 +9,55 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
     secondary = PurpleGrey80,
-    tertiary = Pink80
+    tertiary = Pink80,
+    background = Color.Black, // Pour une couleur de fond sombre
+    surface = Rebonnte_black, // Couleur de surface sombre
+    onPrimary = Color.White, // Couleur du texte sur les éléments primaires
+    onSurface = Color.White, // Couleur du texte sur la surface sombre
+    onSurfaceVariant = Color.Gray // Couleur de texte pour les variantes de surface
+
 )
 
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
-    tertiary = Pink40
+    tertiary = Pink40,
+    background = Color.White, // Fond clair
+    surface = Color(0xFFF5F5F5), // Surface claire
+    onPrimary = Color.Black, // Texte sombre sur élément primaire
+    onSurface = Color.Black, // Texte sombre sur surface
+    onSurfaceVariant = Color.DarkGray // Variante de texte sombre
 )
 
 @Composable
 fun RebonnteTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
+    val systemUiController = rememberSystemUiController()
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
             val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+            dynamicDarkColorScheme(context)
         }
 
         darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+        else -> DarkColorScheme
+    }
+    LaunchedEffect(darkTheme) {
+        systemUiController.setSystemBarsColor(
+            color = DarkColorScheme.background
+        )
     }
 
     MaterialTheme(
