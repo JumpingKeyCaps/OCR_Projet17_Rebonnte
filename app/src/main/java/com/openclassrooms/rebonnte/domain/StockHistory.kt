ee1d64740b8a7092330e809b9958ffe403ad892e
Samuel Lebailly <sam.meusa@gmail.com>
2025-03-14 00:02:29 +0100

    feature(RA-4): Upgrade of the StockHistory data model.

--- app/src/main/java/com/openclassrooms/rebonnte/domain/StockHistory.kt
+++ app/src/main/java/com/openclassrooms/rebonnte/domain/StockHistory.kt
@@ -6,10 +6,11 @@ import java.io.Serializable
 @Keep
 data class StockHistory(
     val date: String,
+    val time: String,
     val medicineId: String,
     val medicineName: String,
     val quantity: Long,
-    val reason: String,
-    val type: String, // "ADD" ou "REMOVE"
-    val updatedBy: String
+    val action: String,// "ADD" ou "REMOVE"
+    val description: String,
+    val author: String
 ) : Serializable
