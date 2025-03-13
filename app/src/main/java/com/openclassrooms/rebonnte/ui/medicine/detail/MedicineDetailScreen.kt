88d84217a87a7b8fb6cddfa6dd872da2c5356d61
Samuel Lebailly <sam.meusa@gmail.com>
2025-03-13 19:28:25 +0100

    feature(RA-75): Remove HistoryItem composition + Inject viewmodel.

--- app/src/main/java/com/openclassrooms/rebonnte/ui/medicine/detail/MedicineDetailScreen.kt
+++ app/src/main/java/com/openclassrooms/rebonnte/ui/medicine/detail/MedicineDetailScreen.kt
@@ -1,22 +1,14 @@
-package com.openclassrooms.rebonnte.ui.medicine
+package com.openclassrooms.rebonnte.ui.medicine.detail
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.KeyboardArrowUp
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -26,39 +18,17 @@ import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.ViewModelProvider
-import com.openclassrooms.rebonnte.MainActivity
-import com.openclassrooms.rebonnte.ui.history.History
-import com.openclassrooms.rebonnte.ui.theme.RebonnteTheme
-import dagger.hilt.android.AndroidEntryPoint
-import java.util.Date
-@AndroidEntryPoint
-class MedicineDetailActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        val name = intent.getStringExtra("nameMedicine") ?: "Unknown"
-        val viewModel = ViewModelProvider(MainActivity.mainActivity)[MedicineViewModel::class.java]
-
-        setContent {
-            RebonnteTheme {
-                MedicineDetailScreen(name, viewModel)
-            }
-        }
-    }
-}
+import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
-fun MedicineDetailScreen(name: String, viewModel: MedicineViewModel) {
-    val medicines by viewModel.medicines.collectAsState(initial = emptyList())
-    val medicine = medicines.find { it.name == name } ?: return
-  //  var stock by remember { mutableStateOf(medicine.stock) }
+fun MedicineDetailScreen(medicineId: String, viewModel: MedicineDetailViewModel = hiltViewModel()) {
+
+   viewModel.loadMedicine(medicineId)
+    val medicine by viewModel.medicine.collectAsState()
+    //  var stock by remember { mutableStateOf(medicine.stock) }
 
     Scaffold { paddingValues ->
         Column(
@@ -87,7 +57,7 @@ fun MedicineDetailScreen(name: String, viewModel: MedicineViewModel) {
                 modifier = Modifier.fillMaxWidth()
             ) {
                 IconButton(onClick = {
-                   //todo UPDATE THE STOCK -
+                    //todo UPDATE THE STOCK -
                 }) {
                     Icon(
                         imageVector = Icons.Filled.KeyboardArrowDown,
@@ -113,28 +83,11 @@ fun MedicineDetailScreen(name: String, viewModel: MedicineViewModel) {
             Spacer(modifier = Modifier.height(16.dp))
             Text(text = "History", style = MaterialTheme.typography.titleLarge)
             Spacer(modifier = Modifier.height(8.dp))
-         //   LazyColumn(modifier = Modifier.fillMaxSize()) {
-          //      items(medicine.histories) { history ->
-          //          HistoryItem(history = history)
-          //      }
-          //  }
-        }
-    }
-}
-
-@Composable
-fun HistoryItem(history: History) {
-    Card(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(vertical = 4.dp),
-        elevation = CardDefaults.cardElevation(4.dp)
-    ) {
-        Column(modifier = Modifier.padding(16.dp)) {
-            Text(text = history.medicineName, fontWeight = FontWeight.Bold)
-            Text(text = "User: ${history.userId}")
-            Text(text = "Date: ${history.date}")
-            Text(text = "Details: ${history.details}")
+            //   LazyColumn(modifier = Modifier.fillMaxSize()) {
+            //      items(medicine.histories) { history ->
+            //          HistoryItem(history = history)
+            //      }
+            //  }
         }
     }
 }
