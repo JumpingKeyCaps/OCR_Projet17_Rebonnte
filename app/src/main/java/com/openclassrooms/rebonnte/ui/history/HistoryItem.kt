8451ec20d3f9c2df6417990cfe11604c52176ca8
Samuel Lebailly <sam.meusa@gmail.com>
2025-03-14 11:12:15 +0100

    feature(RA-58): Add new UI data elements + Documentation

--- app/src/main/java/com/openclassrooms/rebonnte/ui/history/HistoryItem.kt
+++ app/src/main/java/com/openclassrooms/rebonnte/ui/history/HistoryItem.kt
@@ -10,10 +10,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import com.openclassrooms.rebonnte.domain.StockHistory
 
-
+/**
+ * Composable function to display a history item in the UI.
+ * @param history The history item to display.
+ */
 @Composable
-fun HistoryItem(history: History) {
+fun HistoryItem(history: StockHistory) {
     Card(
         modifier = Modifier
             .fillMaxWidth()
@@ -22,9 +26,13 @@ fun HistoryItem(history: History) {
     ) {
         Column(modifier = Modifier.padding(16.dp)) {
             Text(text = history.medicineName, fontWeight = FontWeight.Bold)
-            Text(text = "User: ${history.userId}")
             Text(text = "Date: ${history.date}")
-            Text(text = "Details: ${history.details}")
+            Text(text = "Time: ${history.time}")
+            Text(text = "Details: ${history.description}")
+            Text(text = "Quantity: ${history.quantity}")
+            Text(text = "Action: ${history.action}")
+            Text(text = "User: ${history.author}")
+
         }
     }
 }
