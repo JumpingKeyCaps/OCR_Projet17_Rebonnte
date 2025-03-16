68ba50f6d04223817336d4d6910c6949c61dbced
Samuel Lebailly <sam.meusa@gmail.com>
2025-03-16 22:40:10 +0100

    feature(RA-34): Add documentation.

--- app/src/main/java/com/openclassrooms/rebonnte/domain/Aisle.kt
+++ app/src/main/java/com/openclassrooms/rebonnte/domain/Aisle.kt
@@ -2,6 +2,9 @@ package com.openclassrooms.rebonnte.domain
 
 import androidx.annotation.Keep
 
+/**
+ * Data class representing an aisle.
+ */
 @Keep
 data class Aisle(
     val aisleId: String = "",
